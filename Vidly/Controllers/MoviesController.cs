64994e295cdd468589993eebea30a4252cf18d5a
whuang8 <huang554@purdue.edu>
2016-09-01 16:02:22 -0400

    Add route for movie search by release

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -16,18 +16,10 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return new EmptyResult();
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
